e4a0be22805eda21a9682304d3159bc75b9f46ef
aryavijay1 <sevakulavijaykumar@gmail.com>
2021-04-26 22:23:16 -0400

    new

---
+++ vi.sh
@@ -0,0 +1,7 @@
+mkdir /home/bob/countries/USA
+mkdir /home/bob/countries/UK
+mkdir /home/bob/countries/India
+vi /home/bob/countries/USA/capital.txt | echo Washington, D.C, London, New Delhi
+vi /home/bob/countries/UK/capital.txt | echo Washington, D.C, London, New Delhi
+vi /home/bob/countries/India/capital.txt | echo Washington, D.C, London, New Delhi
+uptime
